af24189b80ca001a09be09448bd3a0320fc9a73d
Radik Timerbaev <aGGa4843@live.com>
2020-10-26 11:07:22 +0500

    небольшие правки,   изменил кол-во максимальных слов в генерируемом предложении

--- Program.cs
+++ Program.cs
@@ -28,28 +28,13 @@ namespace TextAnalysis
             var text = File.ReadAllText("HarryPotterText.txt");
             var sentences = SentencesParserTask.ParseSentences(text);
             var frequency = FrequencyAnalysisTask.GetMostFrequentNextWords(sentences);
-            //Расскомментируйте этот блок, если хотите выполнить последнюю задачу до первых двух.
-            
-            frequency = new Dictionary<string, string>
-            {
-                {"harry", "potter"},
-                {"potter", "boy" },
-                {"boy", "who" },
-                {"who", "likes" },
-                {"boy who", "survived" },
-                {"survived", "attack" },
-                {"he", "likes" },
-                {"likes", "harry" },
-                {"ron", "likes" },
-                {"wizard", "harry" },
-            };
-            
+
             while (true)
             {
                 Console.Write("Введите первое слово (например, harry): ");
                 var beginning = Console.ReadLine();
                 if (string.IsNullOrEmpty(beginning)) return;
-                var phrase = TextGeneratorTask.ContinuePhrase(frequency, beginning.ToLower(), 10);
+                var phrase = TextGeneratorTask.ContinuePhrase(frequency, beginning.ToLower(), 13);
                 Console.WriteLine(phrase);
             }
         }
